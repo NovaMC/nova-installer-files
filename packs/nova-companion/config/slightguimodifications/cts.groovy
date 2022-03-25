fb094b83301601759fbbe9a9559bd1626272a530
Lui798 <42649201+Lui798@users.noreply.github.com>
2022-03-24 21:41:52 -0500

    Nova companion 2.4 - update for 1.18.2

--- packs/nova-companion/config/slightguimodifications/cts.groovy
+++ packs/nova-companion/config/slightguimodifications/cts.groovy
@@ -5,11 +5,11 @@ mainMenu {
             x = 2
             y { it - 20 }
         }
-        text = literal("Nova Companion 2.3")
+        text = literal("Nova Companion 2.4")
         align = "left"
         color = 0xFFFFFF
         hoveredColor = 0x6DFFFB
         shadow = true
-        onClicked = url("https://discord.gg/m52kfmbqAm")
+        onClicked = url("https://novaserver.xyz/discord-mc")
     }
 }
